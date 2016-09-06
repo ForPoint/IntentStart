5625071e661741e70c9a8c7d1389b92464514e8b
Administrator <429508297@qq.com>
2016-09-06 09:22:04 +0800

    Initial commit

---
+++ app/src/main/java/com/forpoint/intentstart/AnotherActivity.java
@@ -0,0 +1,26 @@
+package com.forpoint.intentstart;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    public void onBackClick(View view) {
+        //Intent intent = new Intent(AnotherActivity.this, MainActivity.class);
+        //startActivity(intent);
+        finish();
+    }
+
+}
